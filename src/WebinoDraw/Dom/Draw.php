269c35e2a565eef7a72b88fe172195dfb638143c
Peter Bacinsky <peter@bacinsky.sk>
2012-10-24 20:20:31 +0200

    Add Dom'Draw.

---
+++ src/WebinoDraw/Dom/Draw.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * Webino (https://github.com/webino/)
+ *
+ * @link        https://github.com/webino/WebinoDraw/ for the canonical source repository
+ * @copyright   Copyright (c) 2012 Peter Bačinský <peter@bacinsky.sk>
+ * @license     New BSD License
+ * @package     WebinoDraw_Dom
+ */
+
+namespace WebinoDraw\Dom;
+
+use WebinoDraw\Exception;
+use WebinoDraw\Dom\NodeList;
+use Zend\Dom\Css2Xpath;
+use Zend\View\Renderer\PhpRenderer;
+
+/**
+ * @package     WebinoDraw
+ * @subpackage  WebinoDraw_Dom
+ */
+class Draw
+{
+    private $renderer;
+    
+    public function __construct(PhpRenderer $renderer)
+    {
+        $this->renderer = $renderer;
+    }
+    
+    /**
+     * Draw XHTML string.
+     *
+     * @param  string $xhtml Valid XHTML string.
+     * @return string Drawed HTML string.
+     */
+    public function draw($xhtml, array $instructions, array $vars)
+    {
+        if (empty($xhtml)) {
+            throw new Exception\InvalidArgumentException(
+                'Expects valid XHTML'
+            );
+        }
+
+        libxml_use_internal_errors(true); // hack HTML5
+        
+        $doc                      = new \DOMDocument;
+        $doc->preserveWhiteSpace  = false;
+        $doc->formatOutput        = false;
+        $doc->substituteEntities  = false;
+        $doc->strictErrorChecking = false;
+        $doc->loadHtml($xhtml);
+        $doc->xpath               = new \DOMXpath($doc);
+        
+        $this->drawDom($doc, $instructions, $vars);
+        return $doc->saveHTML();
+    }
+
+    /**
+     * Draw DOM document.
+     * 
+     * @param  \DOMDocument $doc DOM to modify.
+     * @param  array $instructions List of draw instructions.
+     * @param  array $vars Array of data.
+     * @return \WebinoDraw\Dom\Draw
+     * @throws Exception\InvalidArgumentException
+     * @throws Exception\InvalidInstructionException
+     */
+    public function drawDom(\DOMDocument $doc, array $instructions, array $vars)
+    {
+        if (empty($doc->xpath)) {
+            throw new Exception\InvalidArgumentException(
+                'Expects document with XPATH'
+            );
+        }
+
+        foreach ($instructions as $node) {
+            $spec  = current($node);
+            $xpath = array();
+
+            // skip unmapped instructions
+            if (empty($spec['xpath']) && empty($spec['query'])) continue;
+
+            if (!empty($spec['xpath'])) {
+                if (!is_array($spec['xpath'])) $xpath[] = $spec['xpath'];
+                else $xpath = array_merge($xpath, $spec['xpath']);
+            };
+            if (!empty($spec['query'])) { // transform css query to xpath
+                if (!is_array($spec['query'])) $xpath[] = Css2Xpath::transform($spec['query']);
+                else $xpath = array_merge($xpath, array_map(function($value){
+                    return Css2Xpath::transform($value);
+                }, $spec['query']));
+            }
+
+            if (empty($xpath)) throw new Exception\InvalidInstructionException(
+                sprintf("Option `xpath` expected '%s'", print_r($spec, 1))
+            );
+
+            $xpath = join('|', $xpath);
+            $nodes = $doc->xpath->query($xpath);
+
+            // skip missing node
+            if (!$nodes || !$nodes->length) continue;
+
+            if (empty($spec['helper'])) throw new Exception\InvalidInstructionException(
+                sprintf("Option `helper` expected '%s'", print_r($spec, 1))
+            );
+
+            $plugin = $this->renderer->plugin($spec['helper']);
+            $plugin->setVars($vars);
+            $plugin(new NodeList($nodes), $spec); // invoke helper
+        }
+        return $this;
+    }
+}
