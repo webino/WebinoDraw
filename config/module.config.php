f1463d979ff78547da438ddf4cf1b14d0b995f94
Peter Bacinsky <peter@bacinsky.sk>
2013-03-25 16:41:32 +0100

    Fixed module.config, updated to meet the vendor naming standards

--- config/module.config.php
+++ config/module.config.php
@@ -5,25 +5,26 @@ return array(
         'definition' => array(
             'compiler' => array(
                 __DIR__ . '/../data/di/definition.php',
-                __DIR__ . '/../data/di/MagicForm.definition.php',
+                __DIR__ . '/../data/di/DiForm.definition.php',
             ),
         ),
     ),
     'service_manager' => array(
         'factories' => array(
-            'ViewDrawStrategy' => 'WebinoDraw\Mvc\Service\ViewDrawStrategyFactory',
+            'WebinoDraw' => 'WebinoDraw\Mvc\Service\WebinoDrawFactory',
+            'WebinoDrawStrategy' => 'WebinoDraw\Mvc\Service\DrawStrategyFactory',
         ),
     ),
     'view_helpers' => array(
         'invokables' => array(
-            'MagicFormRow' => 'WebinoDraw\Form\View\Helper\MagicFormRow',
-            'MagicFormElement' => 'WebinoDraw\Form\View\Helper\MagicFormElement',
-            'DrawElement' => 'WebinoDraw\View\Helper\DrawElement',
-            'DrawForm' => 'WebinoDraw\View\Helper\DrawForm',
+            'WebinoDrawFormRow' => 'WebinoDraw\Form\View\Helper\FormRow',
+            'WebinoDrawFormElement' => 'WebinoDraw\Form\View\Helper\FormElement',
+            'WebinoDrawElement' => 'WebinoDraw\View\Helper\DrawElement',
+            'WebinoDrawForm' => 'WebinoDraw\View\Helper\DrawForm',
         ),
     ),
     'view_manager' => array(
         'doctype' => 'XHTML5', // !!!XML REQUIRED
-        'strategies' => array('ViewDrawStrategy'),
+        'strategies' => array('WebinoDrawStrategy'),
     ),
 );
