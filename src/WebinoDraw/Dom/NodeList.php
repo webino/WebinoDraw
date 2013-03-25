659cfb52cfa49fd8fd9dd6c7a657c8c18e2fc1db
Peter Bacinsky <peter@bacinsky.sk>
2013-03-25 16:48:58 +0100

    Fixed Dom\NodeList

--- src/WebinoDraw/Dom/NodeList.php
+++ src/WebinoDraw/Dom/NodeList.php
@@ -1,63 +1,94 @@
 <?php
 /**
- * Webino (http://webino.sk/)
+ * Webino (http://webino.sk)
  *
- * @link        https://github.com/webino/WebinoDraw/ for the canonical source repository
- * @copyright   Copyright (c) 2013 Webino, s. r. o. (http://webino.sk/)
+ * @link        https://github.com/webino/WebinoDraw for the canonical source repository
+ * @copyright   Copyright (c) 2013 Webino, s. r. o. (http://webino.sk)
+ * @author      Peter Bačinský <peter@bacinsky.sk>
  * @license     New BSD License
- * @package     WebinoDraw\Dom
  */
 
 namespace WebinoDraw\Dom;
 
-use WebinoDraw\Exception;
+use ArrayObject;
+use DOMNodeList;
+use IteratorAggregate;
+use IteratorIterator;
+use WebinoDraw\Exception\InvalidArgumentException;
+use WebinoDraw\Exception\RuntimeException;
 use Zend\View\Helper\EscapeHtml;
 
 /**
- * Batch DOMElement manipulation.
- *
- * @category    Webino
- * @package     WebinoDraw\Dom
- * @author      Peter Bačinský <peter@bacinsky.sk>
+ * Batch DOMElement manipulation
  */
-class NodeList implements \IteratorAggregate
+class NodeList implements IteratorAggregate
 {
     /**
-     * @var \IteratorIterator
+     * @var IteratorIterator
      */
-    private $nodeList;
+    protected $nodes;
 
     /**
-     * @var \Zend\View\Helper\EscapeHtml
+     * @var EscapeHtml
      */
-    private $escapeHtml;
+    protected $escapeHtml;
 
     /**
-     * @param  array|DOMNodeList $nodeList DOMNodes in array or DOMNodelist.
+     * @param array|DOMNodeList $nodes DOMNodes in array or DOMNodelist
      * @return void
-     * @throws Exception\InvalidArgumentException
+     * @throws InvalidArgumentException
      */
-    public function __construct($nodeList)
+    public function __construct($nodes = null)
     {
-        if (is_array($nodeList) || $nodeList instanceof \DOMNodeList) {
-            if (is_array($nodeList)) {
-                $nodeList = new \ArrayObject($nodeList);
-            }
-            $this->nodeList = new \IteratorIterator($nodeList);
-            return;
-        }
-        throw new Exception\InvalidArgumentException(
-            'Expected nodeList as array or \DOMNodelist'
-        );
+        (null === $nodes) or
+            $this->setNodes($nodes);
     }
 
     /**
-     * @param  array|DOMNodeList $nodeList DOMNodes in array or DOMNodelist.
-     * @return \WebinoDraw\Dom\NodeList
+     * @param array|DOMNodeList $nodes DOMNodes in array or DOMNodelist
+     * @return NodeList
      */
-    public function createNodeList($nodeList)
+    public function createNodeList($nodes)
     {
-        return new self($nodeList);
+        return new self($nodes);
+    }
+
+    /**
+     * @return IteratorIterator
+     */
+    public function getNodes()
+    {
+        if (null === $this->nodes) {
+            $this->setNodes(array());
+        }
+        return $this->nodes;
+    }
+
+    /**
+     * @param array|DOMNodeList|IteratorIterator $nodes
+     * @return NodeList
+     * @throws InvalidArgumentException
+     */
+    public function setNodes($nodes)
+    {
+        if ($nodes instanceof IteratorIterator) {
+            $this->nodes = $nodes;
+            return $this;
+        }
+
+        if (is_array($nodes)) {
+            $this->nodes = new IteratorIterator(new ArrayObject($nodes));
+            return $this;
+        }
+
+        if ($nodes instanceof DOMNodeList) {
+            $this->nodes = new IteratorIterator($nodes);
+            return $this;
+        }
+
+        throw new InvalidArgumentException(
+            'Expected nodes as array|DOMNodelist'
+        );
     }
 
     /**
@@ -65,150 +96,191 @@ class NodeList implements \IteratorAggregate
      */
     public function getIterator()
     {
-        return $this->nodeList->getInnerIterator();
+        return $this->getNodes()->getInnerIterator();
     }
 
     /**
-     * @return \Zend\View\Helper\EscapeHtml
+     * @return EscapeHtml
      */
     public function getEscapeHtml()
     {
-        if (!$this->escapeHtml) {
+        if (null === $this->escapeHtml) {
             $this->setEscapeHtml(new EscapeHtml);
         }
+
         return $this->escapeHtml;
     }
 
     /**
-     * @param \Zend\View\Helper\EscapeHtml $escapeHtml
+     * @param EscapeHtml $escapeHtml
      */
     public function setEscapeHtml(EscapeHtml $escapeHtml)
     {
         $this->escapeHtml = $escapeHtml;
+        return $this;
     }
 
     /**
-     * Set node text value.
+     * Set nodes text value
      *
-     * @param  string $value
-     * @param  Callable $preSet Modify and return value. Passed parameters $node, $value.
-     * @return \WebinoDraw\Dom\NodeList
+     * @param string $value
+     * @param Callable $preSet Modify and return value, passed parameters $node, $value
+     * @return NodeList
      */
     public function setValue($value, $preSet = null)
     {
         $escapeHtml = $this->getEscapeHtml();
+
         foreach ($this as $node) {
+
             if (is_callable($preSet)) {
+
                 $nodeValue = $preSet($node, $value);
+
             } else {
+
                 $nodeValue = $value;
             }
+
             $node->nodeValue = $escapeHtml($nodeValue);
         }
+
         return $this;
     }
 
     /**
-     * Set html value to nodes.
+     * Set nodes html value
      *
-     * @param  string $xhtml
-     * @param  Callable $preSet Modify and return xhtml. Passed parameters $node, $xhtml.
-     * @return \WebinoDraw\Dom\NodeList
+     * @param string $xhtml
+     * @param Callable $preSet Modify and return xhtml, passed parameters $node, $xhtml
+     * @return NodeList
      */
     public function setHtml($xhtml, $preSet = null)
     {
         foreach ($this as $node) {
+
             if (is_callable($preSet)) {
+
                 $nodeXhtml = $preSet($node, $xhtml);
+
             } else {
+
                 $nodeXhtml = $xhtml;
             }
+
             $node->nodeValue = '';
+
             $frag = $node->ownerDocument->createDocumentFragment();
             $frag->appendXml($nodeXhtml);
+
             $node->appendChild($frag);
         }
+
         return $this;
     }
 
     /**
-     * Set attributes to nodes.
+     * Set nodes attributes
      *
-     * @param  array $attribs Attributes to set.
-     * @param  Callable $preSet Modify and return value. Passed parameters $node, $value.
-     * @return \WebinoDraw\Dom\NodeList
-     * @throws Exception\RuntimeException
+     * @param  array $attribs Attributes to set
+     * @param  Callable $preSet Modify and return value, assed parameters $node, $value
+     * @return NodeList
+     * @throws RuntimeException
      */
     public function setAttribs(array $attribs, $preSet = null)
     {
         foreach ($this as $node) {
+
             foreach ($attribs as $name => $value) {
+
                 if (is_callable($preSet)) {
+
                     $value = $preSet($node, $value);
                 }
+
                 if (empty($value) && !is_numeric($value)) {
+
                     $node->removeAttribute($name);
+
                 } else {
+
                     $node->setAttribute($name, trim($value));
                 }
             }
         }
+
         return $this;
     }
 
     /**
-     * Replace node with XHTML code.
+     * Replace node with XHTML code
      *
-     * @param  string $xhtml XHTML to replace node.
-     * @param  Callable $preSet Modify and return xhtml. Passed parameters $node, $xhtml.
-     * @return \WebinoDraw\Dom\NodeList
+     * @param string $xhtml XHTML to replace node
+     * @param Callable $preSet Modify and return xhtml, assed parameters $node, $xhtml
+     * @return NodeList
      */
     public function replace($xhtml, $preSet = null)
     {
         $remove   = array();
-        $nodeList = new \ArrayObject;
+        $nodeList = new ArrayObject;
+
         foreach ($this as $node) {
+
             if (is_callable($preSet)) {
+
                 $nodeXhtml = $preSet($node, $xhtml);
+
             } else {
+
                 $nodeXhtml = $xhtml;
             }
+
             $frag = $node->ownerDocument->createDocumentFragment();
             $frag->appendXml($nodeXhtml);
+
             $nodeList[] = $node->parentNode->insertBefore($frag, $node);
-            $remove[] = $node;
+            $remove[]   = $node;
         }
+
         foreach ($remove as $node) {
             $node->parentNode->removeChild($node);
         }
-        $this->nodeList = new \IteratorIterator($nodeList);
+
+        $this->nodes = new IteratorIterator($nodeList);
+
         return $this;
     }
 
     /**
-     * Remove nodes mapped by xpath.
+     * Remove target nodes
      *
      * @param string $xpath
-     * @return \WebinoDraw\Dom\NodeList
-     * @throws Exception\RuntimeException
+     * @return NodeList
+     * @throws RuntimeException
      */
     public function remove($xpath = '.')
     {
         $remove = array();
+
         foreach ($this as $node) {
+
             if (empty($node->ownerDocument->xpath)) {
-                throw new Exception\RuntimeException(
-                    'Expects DOMDocument with XPath'
+                throw new RuntimeException(
+                    'Expects DOMDocument with xpath'
                 );
             }
+
             $nodes = $node->ownerDocument->xpath->query($xpath, $node);
+
             foreach ($nodes as $subnode) {
                 $remove[] = $subnode;
             }
         }
+
         foreach ($remove as $node) {
             $node->parentNode->removeChild($node);
         }
+
         return $this;
     }
 }
