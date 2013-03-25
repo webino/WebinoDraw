b545ae7848b8d515ee257037bfac0440f3c57d90
Peter Bacinsky <peter@bacinsky.sk>
2013-03-25 17:00:25 +0100

    Added DrawTranslation

---
+++ src/WebinoDraw/Stdlib/DrawTranslation.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Webino (http://webino.sk)
+ *
+ * @link        https://github.com/webino/WebinoDraw for the canonical source repository
+ * @copyright   Copyright (c) 2013 Webino, s. r. o. (http://webino.sk)
+ * @author      Peter Bačinský <peter@bacinsky.sk>
+ * @license     New BSD License
+ */
+
+namespace WebinoDraw\Stdlib;
+
+use ArrayObject;
+
+/**
+ *
+ */
+class DrawTranslation extends ArrayObject implements
+    ArrayFetchInterface,
+    ArrayMergeInterface
+{
+    /**
+     * Return value in depth from multidimensional array
+     *
+     * @param string $basepath Something like: value.in.the.depth
+     * @return array Result value
+     */
+    public function fetch($basepath)
+    {
+        $value = $this->getArrayCopy();
+        $parts = explode('.', $basepath);
+
+        foreach ($parts as $key) {
+
+            // undefined
+            if (empty($value[$key])) {
+
+                $value = null;
+                break;
+            }
+
+            $value = &$value[$key];
+        }
+
+        return $value;
+    }
+
+    /**
+     * @param array $array
+     * @return DrawTranslation
+     */
+    public function merge(array $array)
+    {
+        $this->exchangeArray(
+            array_merge(
+                $this->getArrayCopy(),
+                $array
+            )
+        );
+
+        return $this;
+    }
+}
