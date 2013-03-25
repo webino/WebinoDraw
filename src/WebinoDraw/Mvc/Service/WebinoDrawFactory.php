8347b45980eb2ead4257d39a6f367a0b1c58864c
Peter Bacinsky <peter@bacinsky.sk>
2013-03-25 16:53:54 +0100

    Added WebinoDrawFactory

---
+++ src/WebinoDraw/Mvc/Service/WebinoDrawFactory.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Webino (http://webino.sk)
+ *
+ * @link        https://github.com/webino/WebinoDraw for the canonical source repository
+ * @copyright   Copyright (c) 2013 Webino, s. r. o. (http://webino.sk)
+ * @author      Peter Bačinský <peter@bacinsky.sk>
+ * @license     New BSD License
+ */
+
+namespace WebinoDraw\Mvc\Service;
+
+use WebinoDraw\WebinoDraw;
+use WebinoDraw\WebinoDrawOptions;
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+/**
+ *
+ */
+class WebinoDrawFactory implements FactoryInterface
+{
+    /**
+     * @param  ServiceLocatorInterface $services
+     * @return WebinoDraw
+     */
+    public function createService(ServiceLocatorInterface $services)
+    {
+        $config = $services->get('Config');
+
+        return new WebinoDraw(
+            $services->get('ViewRenderer'),
+            new WebinoDrawOptions($config['webino_draw'])
+        );
+    }
+}
