b28c25e10419894fb6a0a1dba83f3fc68e59dca9
Peter Bacinsky <peter@bacinsky.sk>
2013-03-25 16:49:25 +0100

    Enhanced DrawEvent

--- src/WebinoDraw/DrawEvent.php
+++ src/WebinoDraw/DrawEvent.php
@@ -1,42 +1,106 @@
 <?php
+/**
+ * Webino (http://webino.sk)
+ *
+ * @link        https://github.com/webino/WebinoDraw for the canonical source repository
+ * @copyright   Copyright (c) 2013 Webino, s. r. o. (http://webino.sk)
+ * @author      Peter Bačinský <peter@bacinsky.sk>
+ * @license     New BSD License
+ */
 
 namespace WebinoDraw;
 
+use ArrayObject;
+use WebinoDraw\Dom\NodeList;
 use Zend\EventManager\Event;
 
+/**
+ *
+ */
 class DrawEvent extends Event
 {
-    public function getNode()
+    /**
+     * @var NodeList
+     */
+    protected $nodes;
+
+    /**
+     * @var ArrayObject
+     */
+    protected $spec;
+
+    /**
+     * @return NodeList
+     */
+    public function getNodes()
     {
-        return $this->getParam('node');
+        if (null == $this->nodes) {
+            $this->setNodes(new NodeList);
+        }
+
+        return $this->nodes;
     }
 
-    public function setNode(\DOMNode $node)
+    /**
+     * @param NodeList $nodes
+     * @return DrawEvent
+     */
+    public function setNodes(NodeList $nodes)
     {
-        $this->setParam('node', $node);
+        $this->setParam('nodes', $nodes);
+        $this->nodes = $nodes;
         return $this;
     }
 
+    /**
+     * @return ArrayObject
+     */
     public function getSpec()
     {
-        return $this->getParam('spec');
+        if (null === $this->spec) {
+            $this->setSpec(new ArrayObject);
+        }
+
+        return $this->spec;
     }
 
-    public function setSpec(array $spec)
+    /**
+     * @param array|ArrayObject $spec
+     * @return DrawEvent
+     */
+    public function setSpec($spec)
     {
-        $this->setParam(
-            'spec',
-            array_replace_recursive(
-                $this->getParam('spec', array()),
-                $spec
-            )
+        if (is_array($spec)) {
+
+            $this->spec = $this->getSpec();
+            $this->setParam('spec', $this->spec);
+
+            $this->spec->exchangeArray(
+                array_replace_recursive(
+                    $this->spec->getArrayCopy(),
+                    $spec
+                )
+            );
+            return $this;
+        }
+
+        if ($spec instanceof ArrayObject) {
+            $this->setParam('spec', $spec);
+            $this->spec = $spec;
+            return $this;
+        }
+
+        throw new \UnexpectedValueException(
+            'Expected array|ArrayObject'
         );
-        return $this;
     }
 
+    /**
+     * @return DrawEvent
+     */
     public function clearSpec()
     {
-        $this->setParam('spec', array());
+        $this->spec = null;
         return $this;
     }
 }
