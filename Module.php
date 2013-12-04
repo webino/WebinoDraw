67a441eeffbffb895f6c9f1dca28d9c6c606292c
Peter Bacinsky <peter@bacinsky.sk>
2013-12-04 19:34:54 +0100

    Updated root Module.php, added comments

--- Module.php
+++ Module.php
@@ -1,3 +1,10 @@
 <?php
+/**
+ * Webino (http://webino.sk/)
+ *
+ * @link        https://github.com/webino/WebinoDraw/ for the canonical source repository
+ * @copyright   Copyright (c) 2013 Webino, s. r. o. (http://webino.sk/)
+ * @license     New BSD License
+ */
 
 include __DIR__ . '/src/WebinoDraw/Module.php';
