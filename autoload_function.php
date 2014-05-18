f64b8ee4f86a28356c51e4c86143eaa7e7966b45
Peter Bacinsky <peter@bacinsky.sk>
2014-05-18 03:57:34 +0200

    Removed autoload_register+function files

--- autoload_function.php
+++
@@ -1,8 +0,0 @@
-<?php
-return function ($class) {
-    static $map;
-    if (!$map) $map = include __DIR__ . '/src/WebinoDraw/autoload_classmap.php';
-    if (!isset($map[$class])) return false;
-
-    return include $map[$class];
-};
