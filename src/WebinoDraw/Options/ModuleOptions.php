76e517d05aa3d85032966fdfe8f74f44f35827b4
Peter Bacinsky <peter@bacinsky.sk>
2015-05-14 01:15:25 +0200

    Updated ModuleOptions class, added SERVICE constant

--- src/WebinoDraw/Options/ModuleOptions.php
+++ src/WebinoDraw/Options/ModuleOptions.php
@@ -3,7 +3,7 @@
  * Webino (http://webino.sk)
  *
  * @link        https://github.com/webino/WebinoDraw for the canonical source repository
- * @copyright   Copyright (c) 2012-2014 Webino, s. r. o. (http://webino.sk)
+ * @copyright   Copyright (c) 2012-2015 Webino, s. r. o. (http://webino.sk)
  * @author      Peter Bačinský <peter@bacinsky.sk>
  * @license     BSD-3-Clause
  */
@@ -15,10 +15,15 @@ use WebinoDraw\Instructions\InstructionsInterface;
 use Zend\Stdlib\AbstractOptions;
 
 /**
- *
+ * Class ModuleOptions
  */
 class ModuleOptions extends AbstractOptions
 {
+    /**
+     * Application service name
+     */
+    const SERVICE = 'WebinoDrawOptions';
+
     /**
      * @var InstructionsInterface
      */
