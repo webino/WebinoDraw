ca8fd58dff65d45eea525d4914970e9952e471e7
Peter Bacinsky <peter@bacinsky.sk>
2015-05-14 00:50:51 +0200

    Updated tests init_autoloader.php, use WebinoDev Autoloader class

--- tests/resources/init_autoloader.php
+++ tests/resources/init_autoloader.php
@@ -3,14 +3,15 @@
  * Webino (http://webino.sk)
  *
  * @link        https://github.com/webino/WebinoDraw for the canonical source repository
- * @copyright   Copyright (c) 2012-2014 Webino, s. r. o. (http://webino.sk)
+ * @copyright   Copyright (c) 2012-2015 Webino, s. r. o. (http://webino.sk)
  * @author      Peter Bačinský <peter@bacinsky.sk>
  * @license     BSD-3-Clause
  */
 
-namespace WebinoDraw\Test;
+namespace WebinoDraw;
 
 use RuntimeException;
+use WebinoDev\Test\Autoloader;
 
 /**
  * Initialize vendor autoloader
@@ -20,9 +21,4 @@ if (empty($loader)) {
     throw new RuntimeException('Unable to load. Run `php composer.phar install`.');
 }
 
-$loader->add('Application', __DIR__ . '/src');
-$loader->add('WebinoDraw', __DIR__ . '/src');
-$loader->add('WebinoDraw', __DIR__ . '/../../src');
-$loader->add('WebinoDraw', __DIR__ . '/../functional');
-$loader->add('WebinoDraw', __DIR__ . '/../selenium');
-$loader->add('WebinoDraw', __DIR__ . '/..');
+call_user_func(new Autoloader(__DIR__, __NAMESPACE__), $loader);
