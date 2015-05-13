3ab0b7c54518a58bcd38b1806d4d5dd237311abc
Peter Bacinsky <peter@bacinsky.sk>
2015-05-14 01:13:17 +0200

    Updated DrawService, added SERVICE constant, beautified

--- src/WebinoDraw/Service/DrawService.php
+++ src/WebinoDraw/Service/DrawService.php
@@ -3,7 +3,7 @@
  * Webino (http://webino.sk)
  *
  * @link        https://github.com/webino/WebinoDraw for the canonical source repository
- * @copyright   Copyright (c) 2012-2014 Webino, s. r. o. (http://webino.sk)
+ * @copyright   Copyright (c) 2012-2015 Webino, s. r. o. (http://webino.sk)
  * @author      Peter Bačinský <peter@bacinsky.sk>
  * @license     BSD-3-Clause
  */
@@ -18,10 +18,15 @@ use WebinoDraw\Instructions\InstructionsRenderer;
 use WebinoDraw\Options\ModuleOptions;
 
 /**
- *
+ * Class DrawService
  */
 class DrawService
 {
+    /**
+     * Application service name
+     */
+    const SERVICE = 'WebinoDraw';
+
     /**
      * @var ModuleOptions
      */
@@ -33,12 +38,12 @@ class DrawService
     protected $instructionsRenderer;
 
     /**
-     * @param ModuleOptions $options
-     * @param InstructionsRenderer $instructionsRenderer
+     * @param object|ModuleOptions $options
+     * @param object|InstructionsRenderer $instructionsRenderer
      */
     public function __construct(ModuleOptions $options, InstructionsRenderer $instructionsRenderer)
     {
-        $this->options              = $options;
+        $this->options = $options;
         $this->instructionsRenderer = $instructionsRenderer;
     }
 
