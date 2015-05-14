42b4cca3f229e37a40d7fa1178025123edd15c19
Peter Bacinsky <peter@bacinsky.sk>
2015-05-14 11:06:02 +0200

    Added plugin manager factory (replacing the DI config)

--- src/WebinoDraw/Factory/HelperPluginManagerFactory.php
+++ src/WebinoDraw/Factory/HelperPluginManagerFactory.php
@@ -3,17 +3,19 @@
  * Webino (http://webino.sk)
  *
  * @link        https://github.com/webino/WebinoDraw for the canonical source repository
- * @copyright   Copyright (c) 2012-2014 Webino, s. r. o. (http://webino.sk)
+ * @copyright   Copyright (c) 2012-2015 Webino, s. r. o. (http://webino.sk)
  * @author      Peter Bačinský <peter@bacinsky.sk>
  * @license     BSD-3-Clause
  */
 
 namespace WebinoDraw\Factory;
 
+use WebinoDraw\Draw\HelperPluginManager;
+
 /**
- *
+ * Class HelperPluginManagerFactory
  */
 class HelperPluginManagerFactory extends AbstractPluginManagerFactory
 {
-    const PLUGIN_MANAGER_CLASS = 'WebinoDraw\Draw\HelperPluginManager';
+    const PLUGIN_MANAGER_CLASS = HelperPluginManager::class;
 }
--- src/WebinoDraw/Factory/LoopHelperPluginManagerFactory.php
+++ src/WebinoDraw/Factory/LoopHelperPluginManagerFactory.php
@@ -3,17 +3,19 @@
  * Webino (http://webino.sk)
  *
  * @link        https://github.com/webino/WebinoDraw for the canonical source repository
- * @copyright   Copyright (c) 2012-2014 Webino, s. r. o. (http://webino.sk)
+ * @copyright   Copyright (c) 2012-2015 Webino, s. r. o. (http://webino.sk)
  * @author      Peter Bačinský <peter@bacinsky.sk>
  * @license     BSD-3-Clause
  */
 
 namespace WebinoDraw\Factory;
 
+use WebinoDraw\Draw\LoopHelperPluginManager;
+
 /**
- *
+ * Class LoopHelperPluginManagerFactory
  */
 class LoopHelperPluginManagerFactory extends AbstractPluginManagerFactory
 {
-    const PLUGIN_MANAGER_CLASS = 'WebinoDraw\Draw\LoopHelperPluginManager';
+    const PLUGIN_MANAGER_CLASS = LoopHelperPluginManager::class;
 }
