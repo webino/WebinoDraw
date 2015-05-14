0c6b2b0b0bf84e763a0b17dc52d96b471e557ffd
Peter Bacinsky <peter@bacinsky.sk>
2015-05-14 10:58:18 +0200

    Added ModuleOptionsFactory (replacing the DI config)

--- src/WebinoDraw/Factory/ModuleOptionsFactory.php
+++ src/WebinoDraw/Factory/ModuleOptionsFactory.php
@@ -3,19 +3,20 @@
  * Webino (http://webino.sk)
  *
  * @link        https://github.com/webino/WebinoDraw for the canonical source repository
- * @copyright   Copyright (c) 2012-2014 Webino, s. r. o. (http://webino.sk)
+ * @copyright   Copyright (c) 2012-2015 Webino, s. r. o. (http://webino.sk)
  * @author      Peter Bačinský <peter@bacinsky.sk>
  * @license     BSD-3-Clause
  */
 
 namespace WebinoDraw\Factory;
 
+use WebinoDraw\Factory\InstructionsFactory;
 use WebinoDraw\Options\ModuleOptions;
 use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
 
 /**
- *
+ * Class ModuleOptionsFactory
  */
 class ModuleOptionsFactory implements FactoryInterface
 {
@@ -25,11 +26,11 @@ class ModuleOptionsFactory implements FactoryInterface
      */
     public function createService(ServiceLocatorInterface $services)
     {
-        $config     = $services->get('Config');
+        $config = $services->get('Config');
         $drawConfig = isset($config['webino_draw']) ? $config['webino_draw'] : [];
 
         if (array_key_exists('instructions', $drawConfig)) {
-            $instructionsFactory        = $services->get('WebinoDraw\Factory\InstructionsFactory');
+            $instructionsFactory = $services->get(InstructionsFactory::class);
             $drawConfig['instructions'] = $instructionsFactory->create($drawConfig['instructions']);
         }
 
