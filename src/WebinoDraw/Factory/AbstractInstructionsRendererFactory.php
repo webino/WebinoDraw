667e2a3d50740be6d6f477d72fd1eaf74ae701c0
Peter Bacinsky <peter@bacinsky.sk>
2015-05-14 11:25:16 +0200

    Added InstructionsRendererFactory (replacing the DI config)

---
+++ src/WebinoDraw/Factory/AbstractInstructionsRendererFactory.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Webino (http://webino.sk)
+ *
+ * @link        https://github.com/webino/WebinoDraw for the canonical source repository
+ * @copyright   Copyright (c) 2012-2015 Webino, s. r. o. (http://webino.sk)
+ * @author      Peter Bačinský <peter@bacinsky.sk>
+ * @license     BSD-3-Clause
+ */
+
+namespace WebinoDraw\Factory;
+
+use WebinoDraw\Dom\Factory\NodeListFactory;
+use WebinoDraw\Dom\Locator;
+use WebinoDraw\Factory\InstructionsFactory;
+use WebinoDraw\Instructions\InstructionsRendererInterface;
+use WebinoDraw\Options\ModuleOptions;
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+/**
+ * Class AbstractInstructionsRendererFactory
+ */
+abstract class AbstractInstructionsRendererFactory implements FactoryInterface
+{
+    /**
+     * Instructions renderer class
+     */
+    const ENGINE_CLASS = InstructionsRendererInterface::class;
+
+    /**
+     * @param ServiceLocatorInterface $services
+     * @return InstructionsRendererInterface
+     */
+    public function createService(ServiceLocatorInterface $services)
+    {
+        /** @var Locator $domLocator */
+        $domLocator = $services->get(Locator::class);
+
+        $rendererClass = $this::ENGINE_CLASS;
+        return new $rendererClass(
+            $services->get('WebinoDrawHelperManager'),
+            $domLocator,
+            new NodeListFactory($domLocator),
+            new InstructionsFactory,
+            $services->get(ModuleOptions::SERVICE)
+        );
+    }
+}
---
+++ src/WebinoDraw/Factory/InstructionsRendererFactory.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Webino (http://webino.sk)
+ *
+ * @link        https://github.com/webino/WebinoDraw for the canonical source repository
+ * @copyright   Copyright (c) 2012-2015 Webino, s. r. o. (http://webino.sk)
+ * @author      Peter Bačinský <peter@bacinsky.sk>
+ * @license     BSD-3-Clause
+ */
+
+namespace WebinoDraw\Factory;
+
+use WebinoDraw\Dom\Locator;
+use WebinoDraw\Instructions\InstructionsRenderer;
+
+/**
+ * Class InstructionsRendererFactory
+ */
+class InstructionsRendererFactory extends AbstractInstructionsRendererFactory
+{
+    /**
+     * Instructions renderer class
+     */
+    const ENGINE_CLASS = InstructionsRenderer::class;
+}
