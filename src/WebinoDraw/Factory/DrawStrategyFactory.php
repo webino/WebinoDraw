af409984647da21abefb641bbc2cf87e3addea47
Peter Bacinsky <peter@bacinsky.sk>
2015-05-14 10:56:42 +0200

    Added DrawStrategyFactory (replacing the DI config)

--- src/WebinoDraw/Factory/DrawStrategyFactory.php
+++ src/WebinoDraw/Factory/DrawStrategyFactory.php
@@ -3,7 +3,7 @@
  * Webino (http://webino.sk)
  *
  * @link        https://github.com/webino/WebinoDraw for the canonical source repository
- * @copyright   Copyright (c) 2012-2014 Webino, s. r. o. (http://webino.sk)
+ * @copyright   Copyright (c) 2012-2015 Webino, s. r. o. (http://webino.sk)
  * @author      Peter Bačinský <peter@bacinsky.sk>
  * @license     BSD-3-Clause
  */
@@ -18,13 +18,13 @@ use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
 
 /**
- *
+ * Class DrawStrategyFactory
  */
 class DrawStrategyFactory implements FactoryInterface
 {
     /**
      * @param ServiceLocatorInterface $services
-     * @return AbstractDrawStrategy
+     * @return \WebinoDraw\View\Strategy\AbstractDrawStrategy
      */
     public function createService(ServiceLocatorInterface $services)
     {
