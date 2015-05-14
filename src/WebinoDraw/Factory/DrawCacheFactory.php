b163e677f8652e17b471584765e536596bf7fe5e
Peter Bacinsky <peter@bacinsky.sk>
2015-05-14 10:32:11 +0200

    Added DrawCacheFactory (replacing the DI config)

---
+++ src/WebinoDraw/Factory/DrawCacheFactory.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Webino (http://webino.sk)
+ *
+ * @link        https://github.com/webino/WebinoDraw for the canonical source repository
+ * @copyright   Copyright (c) 2012-2015 Webino, s. r. o. (http://webino.sk)
+ * @author      Peter Bačinský <peter@bacinsky.sk>
+ * @license     BSD-3-Clause
+ */
+
+namespace WebinoDraw\Factory;
+
+use WebinoDraw\Cache\DrawCache;
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+/**
+ * Class DrawCacheFactory
+ */
+class DrawCacheFactory implements FactoryInterface
+{
+    /**
+     * @param ServiceLocatorInterface $services
+     * @return DrawCache
+     */
+    public function createService(ServiceLocatorInterface $services)
+    {
+        return new DrawCache($services->get(DrawCache::STORAGE));
+    }
+}
---
+++ src/WebinoDraw/Factory/DrawCacheStorageFactory.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Webino (http://webino.sk)
+ *
+ * @link        https://github.com/webino/WebinoDraw for the canonical source repository
+ * @copyright   Copyright (c) 2012-2015 Webino, s. r. o. (http://webino.sk)
+ * @author      Peter Bačinský <peter@bacinsky.sk>
+ * @license     BSD-3-Clause
+ */
+
+namespace WebinoDraw\Factory;
+
+use Zend\Cache\StorageFactory;
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+/**
+ * Class DrawCacheStorageFactory
+ */
+class DrawCacheStorageFactory implements FactoryInterface
+{
+    /**
+     * @param ServiceLocatorInterface $services
+     * @return \Zend\Cache\Storage\StorageInterface
+     */
+    public function createService(ServiceLocatorInterface $services)
+    {
+        return StorageFactory::factory([
+            'adapter' => [
+                'name' => 'filesystem',
+                'options' => [
+                    'namespace'      => 'webinodraw',
+                    'cacheDir'       => 'data/cache',
+                    'dirPermission'  => false,
+                    'filePermission' => false,
+                    'umask'          => 7,
+                ],
+            ],
+        ]);
+    }
+}
