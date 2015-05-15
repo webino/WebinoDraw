0c356c72dd23e6e7be7cda47a7cf231dd738ebb8
Peter Bacinsky <peter@bacinsky.sk>
2015-05-15 10:42:15 +0200

    Updated InstructionsFactory copyr. year

--- src/WebinoDraw/Factory/InstructionsFactory.php
+++ src/WebinoDraw/Factory/InstructionsFactory.php
@@ -3,7 +3,7 @@
  * Webino (http://webino.sk)
  *
  * @link        https://github.com/webino/WebinoDraw for the canonical source repository
- * @copyright   Copyright (c) 2012-2014 Webino, s. r. o. (http://webino.sk)
+ * @copyright   Copyright (c) 2012-2015 Webino, s. r. o. (http://webino.sk)
  * @author      Peter Bačinský <peter@bacinsky.sk>
  * @license     BSD-3-Clause
  */
@@ -13,7 +13,7 @@ namespace WebinoDraw\Factory;
 use WebinoDraw\Instructions\Instructions;
 
 /**
- *
+ * Class InstructionsFactory
  */
 class InstructionsFactory
 {
