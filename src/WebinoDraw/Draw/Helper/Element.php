aad24e597490b6a0d302ef11273cb677616faeab
Peter Bacinsky <peter@bacinsky.sk>
2016-02-25 16:10:32 +0100

    Updated Draw\Helper\Element, added trigger_always support

--- src/WebinoDraw/Draw/Helper/Element.php
+++ src/WebinoDraw/Draw/Helper/Element.php
@@ -3,7 +3,7 @@
  * Webino (http://webino.sk)
  *
  * @link        https://github.com/webino/WebinoDraw for the canonical source repository
- * @copyright   Copyright (c) 2012-2015 Webino, s. r. o. (http://webino.sk)
+ * @copyright   Copyright (c) 2012-2016 Webino, s. r. o. (http://webino.sk)
  * @author      Peter Bačinský <peter@bacinsky.sk>
  * @license     BSD-3-Clause
  */
@@ -37,6 +37,9 @@ class Element extends AbstractHelper
 
         $cache = $this->getCache();
         $cacheEvent = clone $event;
+
+        array_key_exists('trigger_always', $spec)
+            and $this->trigger($spec['trigger_always'], $event);
 
         if (empty($spec['loop']) && $cache->load($cacheEvent)) {
             return;
