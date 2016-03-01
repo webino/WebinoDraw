78d57182ac80fc024d38743d85a1690d0a956323
Peter Bacinsky <peter@bacinsky.sk>
2016-03-01 20:57:01 +0100

    Fixed ProfilingInstructionsRenderer::resolveIsNodeDisabled()

--- src/WebinoDraw/Instructions/ProfilingInstructionsRenderer.php
+++ src/WebinoDraw/Instructions/ProfilingInstructionsRenderer.php
@@ -3,7 +3,7 @@
  * Webino (http://webino.sk)
  *
  * @link        https://github.com/webino/WebinoDraw for the canonical source repository
- * @copyright   Copyright (c) 2012-2015 Webino, s. r. o. (http://webino.sk)
+ * @copyright   Copyright (c) 2012-2016 Webino, s. r. o. (http://webino.sk)
  * @author      Peter Bačinský <peter@bacinsky.sk>
  * @license     BSD-3-Clause
  */
@@ -11,7 +11,6 @@
 namespace WebinoDraw\Instructions;
 
 use DOMNodeList;
-use WebinoDraw\Dom\NodeInterface;
 use WebinoDraw\Dom\Locator;
 use WebinoDraw\Exception\LogicException;
 use WebinoDraw\Service\DrawProfiler;
@@ -60,7 +59,7 @@ class ProfilingInstructionsRenderer extends InstructionsRenderer
     /**
      * {@inheritdoc}
      */
-    protected function resolveIsNodeDisabled(NodeInterface $node, array $spec)
+    protected function resolveIsNodeDisabled($node, array $spec)
     {
         $isDisabled = parent::resolveIsNodeDisabled($node, $spec);
         $this->getProfiler()->beginNodeRender($spec, $isDisabled);
