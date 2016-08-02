8704d1cf0d4bb0988ccd1b5753544822db0985a3
Peter Bacinsky <peter@bacinsky.sk>
2016-08-02 19:24:07 +0200

    Fixed DrawFormEvent return typehints

--- src/WebinoDraw/Event/DrawFormEvent.php
+++ src/WebinoDraw/Event/DrawFormEvent.php
@@ -3,7 +3,7 @@
  * Webino (http://webino.sk)
  *
  * @link        https://github.com/webino/WebinoDraw for the canonical source repository
- * @copyright   Copyright (c) 2012-2014 Webino, s. r. o. (http://webino.sk)
+ * @copyright   Copyright (c) 2012-2016 Webino, s. r. o. (http://webino.sk)
  * @author      Peter Bačinský <peter@bacinsky.sk>
  * @license     BSD-3-Clause
  */
@@ -14,7 +14,7 @@ use Zend\Form\Form;
 use Zend\Form\FormInterface;
 
 /**
- *
+ * Class DrawFormEvent
  */
 class DrawFormEvent extends DrawEvent
 {
@@ -36,7 +36,7 @@ class DrawFormEvent extends DrawEvent
 
     /**
      * @param FormInterface $form
-     * @return self
+     * @return $this
      */
     public function setForm(FormInterface $form)
     {
