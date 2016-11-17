b206759bc645d93b07c400337c1ce7ad38197c50
Peter Bacinsky <peter@bacinsky.sk>
2016-11-17 12:36:02 +0100

    Fixed Manipulator\Plugin\Html, call validate() on document before appendXml()

--- src/WebinoDraw/Manipulator/Plugin/Html.php
+++ src/WebinoDraw/Manipulator/Plugin/Html.php
@@ -3,7 +3,7 @@
  * Webino (http://webino.sk)
  *
  * @link        https://github.com/webino/WebinoDraw for the canonical source repository
- * @copyright   Copyright (c) 2012-2014 Webino, s. r. o. (http://webino.sk)
+ * @copyright   Copyright (c) 2012-2016 Webino, s. r. o. (http://webino.sk)
  * @author      Peter Bačinský <peter@bacinsky.sk>
  * @license     BSD-3-Clause
  */
@@ -14,7 +14,7 @@ use DOMElement;
 use WebinoDraw\Exception;
 
 /**
- *
+ * Class Html
  */
 class Html implements InLoopPluginInterface
 {
@@ -42,6 +42,7 @@ class Html implements InLoopPluginInterface
         }
 
         $frag = $node->ownerDocument->createDocumentFragment();
+        $node->ownerDocument->validate();
         $frag->appendXml($translatedHtml);
         if (!$frag->hasChildNodes()) {
             throw new Exception\RuntimeException('Invalid XHTML ' . $translatedHtml);
