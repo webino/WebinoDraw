7a7ba5bf14c6a51d7b36e5a2796709ee32c0fdc5
Peter Bacinsky <peter@bacinsky.sk>
2022-11-28 02:31:37 +0100

    Updated Config\Draw\Variable, added setDefault(), setHelper() methods

--- src/Config/Draw/Variable.php
+++ src/Config/Draw/Variable.php
@@ -8,12 +8,33 @@ namespace Webino\Config\Draw;
 class Variable extends AbstractVariable
 {
     /**
+     * @param mixed $default
+     * @return $this
+     */
+    public function setDefault($default = null): Variable
+    {
+        $this->spec['set'][$this->name] = $default;
+        return $this;
+    }
+
+    /**
      * @param string $path
      * @return $this
      */
     public function setFetch(string $path): Variable
     {
         $this->spec['fetch'][$this->name] = $path;
+        return $this;
+    }
+
+    /**
+     * @param array|object $spec
+     * @return $this
+     */
+    public function setHelper($spec): Variable
+    {
+        // TODO helper object support
+        $this->spec['helper'][$this->name] = is_array($spec) ? $spec : [];
         return $this;
     }
 }
